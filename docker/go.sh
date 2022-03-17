637193df7df6caf74892a785e83d87b8abb340b8
Christopher Adams <adams.christopher.m@gmail.com>
2022-03-17 01:12:26 -0400

    remove django secret key from repo

--- docker/go.sh
+++ docker/go.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 cd /opt/aweber
 
 pip install -r requirements.txt
+
+# django's secret key is necessary, but we won't use it, so no need for it to be consistent.
+# github, however is very unhappy about leaving the secret in the settings file
+export DJANGO_SECRET="$(python -c 'import secrets; print(secrets.token_hex(50))' |head -c 67)"
 
 python manage.py runserver 0.0.0.0:9000
