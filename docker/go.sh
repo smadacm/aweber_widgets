128e9552d62173414d01d7326b968b3c471f818f
Christopher Adams <adams.christopher.m@gmail.com>
2022-03-17 01:45:37 -0400

    run schema migrations automatically

--- docker/go.sh
+++ docker/go.sh
@@ -8,4 +8,9 @@ pip install -r requirements.txt
 # github, however is very unhappy about leaving the secret in the settings file
 export DJANGO_SECRET="$(python -c 'import secrets; print(secrets.token_hex(50))' |head -c 67)"
 
+#python manage.py makemigrations
+# schema changes are stored in VCS, so they can be controlled by commit
+# if changes are there, they're expected to be applied
+python manage.py migrate
+
 python manage.py runserver 0.0.0.0:9000
