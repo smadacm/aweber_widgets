e188c1f513b97c98af6144b743d78fa10cb80c61
Christopher Adams <adams.christopher.m@gmail.com>
2022-03-18 01:04:12 -0400

    refactor views, simplify, add some type hinting bits, fix errors train wreck, comments for readability

--- widgets/models.py
+++ widgets/models.py
@@ -1,26 +1,14 @@
-import json
 import datetime
+import typing
 
 from django.db import models
-
-# TODO: this is trash, dry. need a better way to do this, also need http status associated
-ERR_A_OK = 0
-ERR_MODEL_DOES_NOT_EXIST = 1
-ERR_INVALID_DATA = 2
-ERR_MALFORMED_DATA = 3
-
-possible_errors = {
-    ERR_A_OK: 'Everything is fine.',
-    ERR_MODEL_DOES_NOT_EXIST: 'The requested object does not exist.',
-    ERR_INVALID_DATA: 'The required data was not valid.',
-    ERR_MALFORMED_DATA: 'Unable to parse supplied data.',
-}
 
 NAME_MAX_LENGTH = 64
 
 
-def json_render_field(field):
-    if type(field) in (str, int): return field
+def json_render_field(field: any) -> typing.Union[str, int]:
+    if type(field) in (str, int):
+        return field
     if type(field) == datetime.datetime:
         return field.isoformat()
 
@@ -30,7 +18,7 @@ class Widget(models.Model):
     the entire purpose of this application is to track these do-dads
     id/pk is implied in django
     """
-    name = models.CharField('Name', max_length=NAME_MAX_LENGTH, null=False, blank=False) # TODO: make sure this is utf8, preferably explicitly
+    name = models.CharField('Name', max_length=NAME_MAX_LENGTH, null=False, blank=False)  # TODO: make sure this is utf8, preferably explicitly
     parts_count = models.IntegerField('Number of parts', default=0, null=False, blank=False)
 
     created_at = models.DateTimeField('Created date/time', auto_now_add=True)
@@ -38,23 +26,3 @@ class Widget(models.Model):
 
     def to_dict(self) -> dict:
         return {field.name: getattr(self, field.name) for field in Widget._meta.get_fields()}
-
-
-class Response:
-    """
-    object to jsonify  for response to client
-    reckon this should probably be in a view_models.py, but that seems like over-normalization
-    """
-    def __init__(self, error_number: int = 0, error_message: str = '', widgets: tuple[Widget] = None):
-        self.error_number: int = error_number
-        self.error_message: str = error_message
-        self.widgets: tuple[Widget] = widgets
-
-    def dumps(self):
-        if not self.error_message:
-            self.error_message = possible_errors.get(self.error_number, self.error_message)
-        return json.dumps({
-            'error_number': self.error_number,
-            'error_message': self.error_message,
-            'widgets': tuple(w.to_dict() for w in self.widgets) if self.widgets else (),
-        }, default=json_render_field)
--- widgets/views.py
+++ widgets/views.py
@@ -8,77 +8,93 @@ from django.core.exceptions import ObjectDoesNotExist
 from . import models
 
 
-# TODO: all of this needs to be gone through for readability
-# TODO: move models.Response under render_response(), add HTTP statuses
+# errors
+# format is tuple(error_number, error_message, http_status) where error_number is just an arbitrary int
+ERR_A_OK = (0, 'Everything is fine.', 200)
+ERR_MODEL_DOES_NOT_EXIST = (1, 'The requested object does not exist.', 404)
+ERR_INVALID_DATA = (2, 'The required data was not valid.', 400)
+ERR_MALFORMED_DATA = (3, 'Unable to parse supplied data', 400)
 
-def render_response(error_number: int = 0, error_message: str = '', widgets: tuple[models.Widget] = None):
+
+def render_response(error: tuple[int, str, int] = ERR_A_OK, error_message: str = '',
+                    widgets: tuple[models.Widget] = None) -> HttpResponse:
     """ The entire point of this function is to ensure uniform response. """
 
-    resp_obj = models.Response(error_number=error_number, error_message=error_message, widgets=widgets)
-    return HttpResponse(resp_obj.dumps())
+    error_message = error_message or error[1]
+
+    resp_d = {
+        'error_number': error[0],
+        'error_message': error_message,
+        'widgets': tuple(w.to_dict() for w in widgets) if widgets else (),
+    }
+
+    return HttpResponse(json.dumps(resp_d, default=models.json_render_field), status=error[2])
 
 
-def validate_input(body: str, all_required: bool = False) -> tuple[dict, int, str]:
-    """ Clean input from the user """
-    error_number = 0
-    error_message = ''
-    loaded = None
-
+def _get_widget_or_none(widget_id: int) -> models.Widget:
+    widget = None
     try:
-        loaded = json.loads(body)
-    except json.JSONDecodeError as err:
-        error_number = models.ERR_MALFORMED_DATA
-    else:
-        err_msgs = []
-
-        if all_required:
-            for k in ('name', 'parts_count'):
-                if k not in loaded:
-                    err_msgs.append(f'Missing "{k}" parameter.')
-
-        if len(loaded.get('name', '')) > models.NAME_MAX_LENGTH:
-            err_msgs.append(f'"name" parameter is longer than maximum {models.NAME_MAX_LENGTH} characters.')
-        if type(loaded.get('parts_count', 0)) != int:
-            err_msgs.append('"parts_count" parameter must be an int.')
-
-        if err_msgs:
-            error_number = models.ERR_INVALID_DATA
-            error_message = ' '.join(err_msgs)
-
-    return loaded, error_number, error_message
-
-
-def get_widget_or_none(widget_id: int) -> typing.Optional[models.Widget]:
-    """ mimics dict.get(), but without a modifiable default """
-    possible_widget = None
-    try:
-        possible_widget = models.Widget.objects.get(pk=widget_id)
+        widget = models.Widget.objects.get(pk=widget_id)
     except ObjectDoesNotExist:
-        pass  # will fall back to possible_widgets = None above
-    return possible_widget
+        pass  # widget will be None
+
+    return widget
 
 
-def create_or_update_widget(raw_body: str, widget_id: typing.Optional[int] = None):
+def create_or_update_widget(raw_body: str, widget_id: typing.Optional[int] = None) -> HttpResponse:
     """
     take the raw request body, and possibly a widget id, get or create the widget,
     apply the user's input, save it, and return a response object that can be returned by a view
     """
-    error_number = 0
+    error = ERR_A_OK
     error_message = ''
-    cleaned_input = {} # slightly increases memory footprint, but if there's no default, pycharm fusses
 
-    if widget_id:
-        widget = get_widget_or_none(widget_id)
+    # these 2 slightly increase memory footprint, but if there's no default, pycharm fusses
+    cleaned_input = {}
+    parsed_body = {}
+
+    # get or create widget based on presence of widget_id
+    if widget_id is not None:
+        widget = _get_widget_or_none(widget_id)
         if not widget:
-            error_number = models.ERR_MODEL_DOES_NOT_EXIST
+            error = ERR_MODEL_DOES_NOT_EXIST
     else:
         widget = models.Widget()
 
-    if not error_number:
-        # ensure we have all info needed to create a Widget
-        cleaned_input, error_number, error_message = validate_input(raw_body, widget_id is None)
+    # parse request body to dict
+    if not error:
+        try:
+            parsed_body = json.loads(raw_body)
+        except json.JSONDecodeError as err:
+            error = ERR_MALFORMED_DATA
 
-    if not error_number:
+    # check data is clean
+    if not error:
+        err_msgs = []
+
+        # if this is a new object, require all fields
+        if widget_id is None:
+            for k in ('name', 'parts_count'):
+                if k not in parsed_body:
+                    err_msgs.append(f'Missing "{k}" parameter.')
+
+        # check that parts_count is an int
+        if type(parsed_body.get('parts_count', 0)) != int:
+            err_msgs.append('"parts_count" parameter must be an int.')
+        # check that parts_count is an int
+        if 'name' in parsed_body and type(parsed_body['name']) != str:
+            err_msgs.append('"name" parameter must be a str.')
+        # check name max length
+        elif 'name' in parsed_body and len(parsed_body['name']) > models.NAME_MAX_LENGTH:
+            err_msgs.append(f'"name" parameter is longer than maximum {models.NAME_MAX_LENGTH} characters.')
+
+        # any error message means failure
+        if err_msgs:
+            error = ERR_INVALID_DATA
+            error_message = ' '.join(err_msgs)
+
+    # if we get this far, the input is valid, so update the widget and save
+    if not error:
         if 'name' in cleaned_input:
             widget.name = cleaned_input['name']
         if 'parts_count' in cleaned_input:
@@ -86,10 +102,11 @@ def create_or_update_widget(raw_body: str, widget_id: typing.Optional[int] = Non
 
         widget.save()
 
-    return render_response(error_number=error_number, error_message=error_message or None, widgets=(widget,))
+    # return the error or created/updated widget
+    return render_response(error=error, error_message=error_message or None, widgets=(widget,))
 
 
-# Create your views here.
+# The actual views
 class Widgets(generic.View):
     # list all widgets
     def get(self, request: HttpRequest, *args: [any], **kwargs: dict) -> HttpResponse:
@@ -97,29 +114,26 @@ class Widgets(generic.View):
 
     # create
     def put(self, request: HttpRequest, *args: [any], **kwargs: dict) -> HttpResponse:
-        body = request.body
-        return create_or_update_widget(raw_body=body)
+        return create_or_update_widget(raw_body=request.body)
 
 
 class ExistingWidget(generic.View):
     # read
     def get(self, request: HttpRequest, widget_id: int, *args: [any], **kwargs: dict) -> HttpResponse:
-        possible_widget = get_widget_or_none(widget_id)
-        return render_response(error_number=models.ERR_A_OK if possible_widget else models.ERR_MODEL_DOES_NOT_EXIST,
-                               widgets=possible_widget)
+        widget = _get_widget_or_none(widget_id)
+        return render_response(error=ERR_A_OK if widget else ERR_MODEL_DOES_NOT_EXIST, widgets=(widget,))
 
     # update
     def patch(self, request: HttpRequest, widget_id: int, *args: [any], **kwargs: dict) -> HttpResponse:
-        body = request.body
-        return create_or_update_widget(raw_body=body, widget_id=widget_id)
+        return create_or_update_widget(raw_body=request.body, widget_id=widget_id)
 
     # delete
     def delete(self, request: HttpRequest, widget_id: int, *args: [any], **kwargs: dict) -> HttpResponse:
-        error_number = models.ERR_A_OK
-        widget = get_widget_or_none(widget_id)
+        error = ERR_A_OK
+        widget = _get_widget_or_none(widget_id)
         if not widget:
-            error_number = models.ERR_MODEL_DOES_NOT_EXIST
+            error = ERR_MODEL_DOES_NOT_EXIST
         else:
             widget.delete()
 
-        return render_response(error_number)
+        return render_response(error)
